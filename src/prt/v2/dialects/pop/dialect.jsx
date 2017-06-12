3a16aeac94a409882baa147c02a1fa2fd44b4617
Peter Varo <hello@petervaro.com>
2017-06-12 14:39:55 +0100

    xmlToIdentifier and xmlToAttribute methods and their types have been added; minor changes here and there (#3)

--- src/prt/v2/dialects/pop/dialect.jsx
+++ src/prt/v2/dialects/pop/dialect.jsx
@@ -4,8 +4,10 @@
 import type { PRTIdentifier,
               PRTPlainText } from 'prt/v2/types';
 
-import type { PRTIdentifierToHTML,
-              PRTAttributeToHTML } from 'prt/v2/dialect';
+import type { PRTIdentifierToXml,
+              PRTXmlToIdentifier,
+              PRTAttributeToXml,
+              PRTXmlToAttribute } from 'prt/v2/dialect';
 
 /* Import PRT objects */
 import PRTError   from 'prt/error';
@@ -16,9 +18,18 @@ import PRTDialect from 'prt/v2/dialect';
 export const PRTPOPInvalidIdentifier = function (identifier: PRTIdentifier) {
   this.message = `Invalid identifier for pop dialect: ` +
                  `${identifier} (type ${typeof identifier})`;
-}
+};
 PRTPOPInvalidIdentifier.prototype      = Object.create(PRTError.prototype);
 PRTPOPInvalidIdentifier.prototype.name = 'PRTPOPInvalidIdentifier';
+
+
+/*----------------------------------------------------------------------------*/
+export const PRTPOPInvalidXMLTag = function (tag: PRTPlainText) {
+  this.message = `Invalid XML tag for pop dialect: ` +
+                 `${tag.toString()} (type ${typeof tag})`;
+};
+PRTPOPInvalidXMLTag.prototype      = Object.create(PRTError.prototype);
+PRTPOPInvalidXMLTag.prototype.name = 'PRTPOPInvalidXMLTag';
 
 
 /*----------------------------------------------------------------------------*/
@@ -26,16 +37,26 @@ export const PRTPOPInvalidAttribute = function (identifier : PRTPlainText,
                                                 attribute  : PRTPlainText) {
   this.message = `Invalid attribute for identifier "${identifier.toString()}"` +
                 `: ${attribute.toString()} (type ${typeof attribute})`;
-}
+};
 PRTPOPInvalidAttribute.prototype      = Object.create(PRTError.prototype);
 PRTPOPInvalidAttribute.prototype.name = 'PRTPOPInvalidAttribute';
+
+
+/*----------------------------------------------------------------------------*/
+export const PRTPOPInvalidXMLAttribute = function (identifier : PRTPlainText,
+                                                   attribute  : PRTPlainText) {
+  this.message = `Invalid attribute for identifier "${identifier.toString()}"` +
+                `: ${attribute.toString()} (type ${typeof attribute})`;
+};
+PRTPOPInvalidXMLAttribute.prototype      = Object.create(PRTError.prototype);
+PRTPOPInvalidXMLAttribute.prototype.name = 'PRTPOPInvalidXMLAttribute';
 
 
 /*----------------------------------------------------------------------------*/
 class PRTPOPDialect extends PRTDialect {
 
   /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-  identifierToHTML: PRTIdentifierToHTML = identifier => {
+  identifierToXml: PRTIdentifierToXml = identifier => {
     switch (identifier) {
       /* Anchor */
       case 0x00: return 'a';
@@ -70,9 +91,44 @@ class PRTPOPDialect extends PRTDialect {
   };
 
   /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-  attributeToHTML: PRTAttributeToHTML = (identifier, name, value) => {
+  xmlToIdentifier: PRTXmlToIdentifier = tag => {
+    switch (tag) {
+      /* Anchor */
+      case 'a'    : return 0x00;
+      /* Bold */
+      case 'b'    : return 0x01;
+      /* Code */
+      case 'code' : return 0x02;
+      /* Headers */
+      case 'h1'   : return 0x03;
+      case 'h2'   : return 0x04;
+      case 'h3'   : return 0x05;
+      case 'h4'   : return 0x06;
+      case 'h5'   : return 0x07;
+      case 'h6'   : return 0x08;
+      case 'h7'   : return 0x09;
+      /* Italic */
+      case 'i'    : return 0x0A;
+      /* Image */
+      case 'img'  : return 0x0B;
+      /* Paragraph */
+      case 'p'    : return 0x0C;
+      /* Preformatted */
+      case 'pre'  : return 0x0D;
+      /* Strikethrough */
+      case 's'    : return 0x0E;
+      /* Underline */
+      case 'u'    : return 0x0F;
+      /* Unknown */
+      default:
+        throw new PRTPOPInvalidXMLTag(tag);
+    }
+  }
+
+  /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  attributeToXml: PRTAttributeToXml = (identifier, name, value) => {
     const invalid = () => {
-      throw new PRTPOPInvalidAttribute(this.identifierToHTML(identifier), name);
+      throw new PRTPOPInvalidAttribute(this.identifierToXml(identifier), name);
     };
     /* If not generic attributes */
     if (name !== 'id' ||
@@ -98,6 +154,36 @@ class PRTPOPDialect extends PRTDialect {
     /* Return validated name and value */
     return [name, value];
   };
+
+  /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  xmlToAttribute: PRTXmlToAttribute = (tag, name, value) => {
+    const invalid = () => {
+      throw new PRTPOPInvalidXMLAttribute(this.xmlToIdentifier(tag), name);
+    };
+    /* If not generic attributes */
+    if (name !== 'id' ||
+        name !== 'class') {
+      switch (identifier) {
+        /* Anchor */
+        case 'a':
+          if (name === 'href') {
+            invalid();
+          }
+          break;
+        /* Image */
+        case 'img':
+          if (name !== 'alt' ||
+              name !== 'src') {
+            invalid();
+          }
+          break;
+        default:
+          invalid();
+      }
+    }
+    /* Return validated name and value */
+    return [name, value];
+  }
 }
 
 

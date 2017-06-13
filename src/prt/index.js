f1f704728141eae890f0e5fa912a03a26a3140ce
dbertella <daniele.bertella@gmail.com>
2017-06-13 11:07:14 +0100

    Add build scripts, add babel plugin resolver, add index in the prt root folder

---
+++ src/prt/index.js
@@ -0,0 +1,13 @@
+// @flow
+/* Import components */
+import PRTComponent from 'prt/component';
+import registerPRTDialectByNameAndVersion, {
+  getPRTDialectByNameAndVersion
+} from 'prt/dialects';
+
+/* Export everything */
+export default PRTComponent;
+export {
+  getPRTDialectByNameAndVersion,
+  registerPRTDialectByNameAndVersion
+};
